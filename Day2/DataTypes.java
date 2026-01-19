84c998b28651e1c6bf437ed1ba80e3f7d4274367
tanayupadhyay911-glitch <tanayupadhyay911@gmail.com>
2026-01-19 21:11:27 +0530

    Day 1 Day 2 Added

---
+++ Day2/DataTypes.java
@@ -0,0 +1,45 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+        
+
+        byte myByte = 100;
+        System.out.println("byte: " + myByte);
+
+       
+        short myShort = 5000;
+        System.out.println("short: " + myShort);
+
+        
+        int myInt = 750000;
+        System.out.println("int: " + myInt);
+
+        
+        long myLong = 123456789012345L;
+        System.out.println("long: " + myLong);
+
+       
+        float myFloat = 19.99f;
+        System.out.println("float: " + myFloat);
+
+        
+        double myDouble = 123.456789;
+        System.out.println("double: " + myDouble);
+
+        // --- Other Primitive Types ---
+
+       
+        boolean myBoolean = true;
+        System.out.println("boolean: " + myBoolean);
+
+        
+        char myChar = 'A';
+        System.out.println("char: " + myChar);
+
+        // --- Object Type (String) ---
+
+        
+        String myString = "Hello, Java Developer!";
+        System.out.println("String: " + myString);
+    }
+}
