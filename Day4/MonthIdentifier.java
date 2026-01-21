95b5d4558578367c07c41238b253f8546ffa9321
tanayupadhyay911-glitch <tanayupadhyay911@gmail.com>
2026-01-21 16:59:07 +0530

    Add files via upload
    Added Day04

---
+++ Day4/MonthIdentifier.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class MonthIdentifier {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number between 1 and 12: ");
+
+        
+        if (scanner.hasNextInt()) {
+            int monthNumber = scanner.nextInt();
+
+            String monthName;
+
+           
+            switch (monthNumber) {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+                    break;
+                default:
+                    monthName = "Invalid month number";
+                    break;
+            }
+
+            System.out.println("Month: " + monthName);
+
+        } else {
+            System.out.println("Invalid input. Please enter a valid number.");
+        }
+
+        
+        scanner.close();
+    }
+}
---
+++ Day4/VotingEligibility.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter your age: ");
+
+        
+        int age = scanner.nextInt();
+
+        
+        if (age >= 18) {
+            System.out.println("You are eligible to vote!"); // Output if eligible
+        } else {
+            System.out.println("You are not eligible to vote yet."); // Output if not eligible
+        }
+
+        
+        scanner.close();
+    }
+}
